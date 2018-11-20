847a9353eb554047d9b42709e85a3a8d55df5c35
Walter Blair <wblair@TRHCs-MBP-4.attlocal.net>
2018-11-19 22:34:46 -0500

    fix logic of config building

--- brain/sql.go
+++ brain/sql.go
@@ -8,6 +8,8 @@ import (
 )
 
 type DBClient interface {
+	FetchNumInputs(int) (int, error)
+	SetComponentStatus(int, string) error
 	FetchConfig(Config) (int, []int, string, error)
 	HealthCheck() error
 }
@@ -29,12 +31,11 @@ func NewDBClient(url string) (client DBClient, err error) {
 	return client, err
 }
 
-func (c *DBClientImpl) fetchNumInputs(id int) (int, error) {
+// TODO factor out query and make this general FetchCount function
+// FetchNumInputs returns number of inputs that this component should expect to receive in current configuration
+func (c *DBClientImpl) FetchNumInputs(id int) (count int, err error) {
 
 	var rows		*sql.Rows
-	var row 		ConfigRecord
-	var numInputs	int
-	var err			error
 
 	query := fmt.Sprintf(
 		"SELECT COUNT(*) FROM configurations c " +
@@ -46,28 +47,24 @@ func (c *DBClientImpl) fetchNumInputs(id int) (int, error) {
 	rows, err = c.DB.Query(query)
 
 	if err != nil {
-		log.Println("error fetching config from database")
-		return numInputs, err
+		log.Println("error fetching count from database")
+		return count, err
 	} else {
 		defer rows.Close()
 	}
 
 	for rows.Next() {
-		if err = rows.Scan(&row.NumInputs); err != nil {
+		if err = rows.Scan(&count); err != nil {
 			log.Println("error unmarshalling query result row: ", err.Error())
-			return numInputs, err
+			return count, err
 		}
 	}
 
-	if err = rows.Err(); err != nil {
-		log.Println("error parsing query result rows")
-		return numInputs, err
-	}
-
-	return numInputs, err
+	return count, err
 }
 
-func (c *DBClientImpl) setComponentStatus(id int, status string) (error) {
+// TODO factor out query and make this general Update function
+func (c *DBClientImpl) SetComponentStatus(id int, status string) error {
 	query := fmt.Sprintf(
 		"UPDATE configurations c " +
 		"SET status = '%s' " +
@@ -104,35 +101,30 @@ func (c *DBClientImpl) FetchConfig(config Config) (int, []int, string, error) {
 	}
 
 	for rows.Next() {
-
 		if err := rows.Scan(&row.Function, &row.NextKey); err != nil {
-			log.Println("error unmarshalling query result row: ")
+			log.Println("error unmarshalling query result row: ", err.Error())
 			return numInputs, nextKeys, row.Function, err
 		}
 		nextKeys = append(nextKeys, row.NextKey)
 	}
-	if err = rows.Err(); err != nil {
-		log.Println("error parsing query result rows")
-		return numInputs, nextKeys, row.Function, err
-	}
 
 	log.Println("retrieved from db config for routing key: ", id)
 
-	log.Println("determining number of inputs...")
-	numInputs, err = c.fetchNumInputs(id)
-	if err != nil {
-		log.Println("error determining number of inputs")
-		return numInputs, nextKeys, row.Function, err
-	}
-	log.Printf("number of inputs for %d: %d", id, numInputs)
-
 	log.Println("updating status for this component in db...")
-	err = c.setComponentStatus(id, config.Status)
+	err = c.SetComponentStatus(id, config.Status)
 	if err != nil {
 		log.Println("error updating status for this component: ", err.Error())
 		return numInputs, nextKeys, row.Function, err
 	}
 	log.Printf("updated status for %d to %s", id, config.Status)
+
+	log.Println("determining number of inputs...")
+	numInputs, err = c.FetchNumInputs(id)
+	if err != nil {
+		log.Println("error determining number of inputs")
+		return numInputs, nextKeys, row.Function, err
+	}
+	log.Printf("number of inputs for %d: %d", id, numInputs)
 
 	return numInputs, nextKeys, row.Function, err
 
--- brain/sql_test.go
+++ brain/sql_test.go
@@ -1,25 +1,60 @@
 package brain_test
 
 import (
+	"database/sql"
+	"github.com/DATA-DOG/go-sqlmock"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/waltermblair/brain/brain"
 )
 
-// TODO - mock this out like core-test
 var _ = Describe("database tests", func() {
 
-	var client DBClient
-	var err    error
-
-	dbURL := "root:root@tcp(localhost:3306)/store"
+	var mockDB     DBClient
+	var db     	   *sql.DB
+	var mock       sqlmock.Sqlmock
 
 	BeforeEach(func() {
-		client, err = NewDBClient(dbURL)
+		db, mock, _ = sqlmock.New()
+		mockDB = &DBClientImpl{
+			"test",
+			db,
+		}
+	})
+	AfterEach(func() {
+		db.Close()
 	})
 
-	PIt("should connect to db", func() {
-		Ω(client.HealthCheck()).Should(BeNil())
+	It("should FetchNumInputs", func() {
+		rows := sqlmock.NewRows([]string{"COUNT(*)"}).AddRow(3)
+		mock.ExpectQuery("^SELECT COUNT").WillReturnRows(rows)
+		count, err := mockDB.FetchNumInputs(0)
+		Ω(count).Should(Equal(3))
+		Ω(err).Should(BeNil())
+		Ω(mock.ExpectationsWereMet()).To(BeNil())
+	})
+
+	It("should SetComponentStatus", func() {
+		result := sqlmock.NewResult(0, 1)
+		mock.ExpectExec("^UPDATE configurations").WillReturnResult(result)
+		err := mockDB.SetComponentStatus(0, "up")
+		Ω(err).Should(BeNil())
+		Ω(mock.ExpectationsWereMet()).To(BeNil())
+	})
+
+	It("should FetchConfig", func() {
+		rows := sqlmock.NewRows([]string{"function", "next"}).AddRow("buffer", 1)
+		result := sqlmock.NewResult(0, 1)
+		rows2 := sqlmock.NewRows([]string{"COUNT(*)"}).AddRow(3)
+		mock.ExpectQuery("^SELECT (.+)").WillReturnRows(rows)
+		mock.ExpectExec("^UPDATE configurations").WillReturnResult(result)
+		mock.ExpectQuery("^SELECT COUNT").WillReturnRows(rows2)
+		numInputs, nextKeys, fn, err := mockDB.FetchConfig(Config{})
+		Ω(numInputs).Should(Equal(3))
+		Ω(nextKeys).Should(Equal([]int{1}))
+		Ω(fn).Should(Equal("buffer"))
+		Ω(err).Should(BeNil())
+		Ω(mock.ExpectationsWereMet()).To(BeNil())
 	})
 
 })
